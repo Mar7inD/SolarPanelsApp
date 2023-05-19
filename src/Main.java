afec910bdc711e845f26e0fa3ee55850c2860086
Mar7inD <Dotev32@gmail.com>
2023-05-19 17:20:45 +0200

    The DatabaseConnection.java is created.

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,24 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import controller.ViewHandler;
 
+import java.sql.SQLException;
+
 public class Main extends Application
 {
-  private ViewHandler viewHandler;
-
   @Override public void start(Stage primaryStage)
   {
-    viewHandler = new ViewHandler(primaryStage);
+    ViewHandler viewHandler = new ViewHandler(primaryStage);
   }
 
   public static void main(String[] args)
   {
     Application.launch(Main.class);
+
+    try (DatabaseConnection dbConnection = new DatabaseConnection()) {
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 
 }
--- src/controller/SolarPanelsSceneController.java
+++ src/controller/SolarPanelsSceneController.java
@@ -2,17 +2,26 @@ package controller;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import javafx.fxml.FXML;
 
 import javafx.event.ActionEvent;
 
+
 public class SolarPanelsSceneController
 {
   @FXML private Button back;
   @FXML private Button cancel;
-  @FXML private ComboBox cSituation;
-  @FXML private ComboBox infrastructure;
+  @FXML private TableView<String> solarPanelsTable;
+  @FXML private TableColumn<String, Integer> serialNo;
+  @FXML private TableColumn<String, String> panelType;
+  @FXML private TableColumn<String, String> roofPosition;
+  @FXML private TableColumn<String, String> installationDate;
+  @FXML private TableColumn<String, String> manufacturer;
+  @FXML private TableColumn<String, String> activity;
+  @FXML private ComboBox solarPanelPosition;
 
   private ViewHandler viewHandler;
 
@@ -25,7 +34,7 @@ public class SolarPanelsSceneController
   {
     if (event.getSource() == back)
     {
-      viewHandler.changeScene(viewHandler.MAIN_SCENE);
+      viewHandler.changeScene(ViewHandler.MAIN_SCENE);
     }
   }
 }
